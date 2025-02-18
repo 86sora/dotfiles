ddebd370e0e05963ec2cf8ddeab581b89d1ceaa4
sora <zeluis30012007@gmail.com>
2025-02-18 19:08:52 +0000

    new hypr and waybar configs and vim lsps added

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
-    local transparent = false --this messes up the colours idk why
+    local transparent = true --this messes up the colours idk why
     
     --colors
     local bg = "#011628"
